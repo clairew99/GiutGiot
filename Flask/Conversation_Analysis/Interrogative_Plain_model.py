b6ac91c95e79401e92ab73597adbe5280b156861
이재성 <commonhm@gmail.com>
2024-08-08 14:13:43 +0900

    feat: flask sync setting

--- Flask/Conversation_Analysis/Interrogative_Plain_model.py
+++ Flask/Conversation_Analysis/Interrogative_Plain_model.py
@@ -1,4 +1,4 @@
-from Interrogative_Plain import *
+from Conversation_Analysis.Interrogative_Plain import *
 # 저장된 모델 불러오기
 classifier = QuestionStatementClassifier()
 classifier.load_model('svc_model.pkl', 'tfidf_vectorizer.pkl')
