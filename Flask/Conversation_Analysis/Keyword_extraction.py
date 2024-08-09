4d400d235541f58b51d5fb6893f271ee39066e36
Sinijini <gmlwls2407@naver.com>
2024-08-09 10:21:20 +0900

    feat: conversation model update

--- Flask/Conversation_Analysis/Keyword_extraction.py
+++ Flask/Conversation_Analysis/Keyword_extraction.py
@@ -1,5 +1,5 @@
 from konlpy.tag import Okt
-from data.Keyword_data import *
+from Conversation_Analysis.data.Keyword_data import *
 
 class ClothingFeatureExtractor:
     def __init__(self):
@@ -8,6 +8,7 @@ class ClothingFeatureExtractor:
             "긴팔": ["기다", "팔"],
             "반팔": ["반", "팔"],
             "긴바지": ["기다", "바지"],
+            "긴팬츠": ["기다", "팬츠"],
             "반바지": ["반", "바지"],
             "후드티": ["후드", "티"],
             "맨투맨": ["맨", "투", "맨"]
@@ -72,6 +73,7 @@ class ClothingFeatureExtractor:
         
         current_part = None
         current_color = None
+        current_pattern = None
         
         for i, (word, pos) in enumerate(tokens):
             if pos in ['Noun', 'Adjective']:
@@ -81,6 +83,11 @@ class ClothingFeatureExtractor:
                 if color:
                     current_color = color
                 
+                # Pattern recognition
+                for pattern, synonyms in patterns.items():
+                    if word in synonyms:
+                        current_pattern = pattern
+                
                 if is_top is not None:
                     if current_color:
                         if is_top:
@@ -88,6 +95,12 @@ class ClothingFeatureExtractor:
                         else:
                             bottom_features['color'] = current_color
                         current_color = None
+                    if current_pattern:
+                        if is_top:
+                            top_features['pattern'] = current_pattern
+                        else:
+                            bottom_features['pattern'] = current_pattern
+                        current_pattern = None
                     current_part = 'top' if is_top else 'bottom'
                 
                 if current_part == 'top' or (current_part is None and is_top is None):
@@ -100,33 +113,42 @@ class ClothingFeatureExtractor:
                             if current_color:
                                 top_features['color'] = current_color
                                 current_color = None
+                            if current_pattern:
+                                top_features['pattern'] = current_pattern
+                                current_pattern = None
                 
                 if current_part == 'bottom' or (current_part is None and is_top is None):
                     for pants_type, synonyms in pants_types.items():
-                        if word in synonyms:
+                        if word in synonyms or '팬츠' in word or '바지' in word:
                             bottom_features['pants_type'] = pants_type
                             if current_color:
                                 bottom_features['color'] = current_color
                                 current_color = None
+                            if current_pattern:
+                                bottom_features['pattern'] = current_pattern
+                                current_pattern = None
                     for bottom_type, synonyms in bottom_types.items():
                         if word in synonyms:
                             bottom_features['bottom_type'] = bottom_type
                             if current_color:
                                 bottom_features['color'] = current_color
                                 current_color = None
-                
-                for pattern, synonyms in patterns.items():
-                    if word in synonyms:
-                        if current_part == 'top':
-                            top_features['pattern'] = pattern
-                        elif current_part == 'bottom':
-                            bottom_features['pattern'] = pattern
+                            if current_pattern:
+                                bottom_features['pattern'] = current_pattern
+                                current_pattern = None
         
-        # If there's a remaining color, assign it to the last known part
-        if current_color:
-            if current_part == 'top':
-                top_features['color'] = current_color
-            elif current_part == 'bottom':
-                bottom_features['color'] = current_color
+        # If there's a remaining color or pattern, assign it to the last known part
+        if current_color or current_pattern:
+            if current_part == 'top' or (current_part is None and bottom_features['pants_type'] is None):
+                if current_color:
+                    top_features['color'] = current_color
+                if current_pattern:
+                    top_features['pattern'] = current_pattern
+            elif current_part == 'bottom' or (current_part is None and bottom_features['pants_type'] is not None):
+                if current_color:
+                    bottom_features['color'] = current_color
+                if current_pattern:
+                    bottom_features['pattern'] = current_pattern
 
         return {'top': top_features, 'bottom': bottom_features}
+
