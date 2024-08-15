be3b31559a5aa53a675f861568b303399fafc350
Sinijini <gmlwls2407@naver.com>
2024-08-15 20:06:02 +0900

    feat: 음성에 따른 라우터 설정

--- Flask/app.py
+++ Flask/app.py
@@ -12,144 +12,81 @@ import schedule
 import time
 import threading
 
-
 app = Flask(__name__)
 app.secret_key = Config.SECRETKEY
 
 # 세션 수명을 30분으로 설정
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
-# ClothingFeatureExtractor 클래스의 인스턴스를 생성합니다.
+# 각 클래스의 인스턴스를 생성
 extractor = ClothingFeatureExtractor()
-
-# AudioProcessor 클래스의 인스턴스를 생성합니다.
 audio_processor = AudioProcessor(hf_token=Config.PYANNOTE_TOKEN)
-audio_files = []
-
-# QuestionStatementClassifier 클래스의 인스턴스를 생성합니다.
 classifier = QuestionStatementClassifier()
 classifier.load_model('svc_model.pkl', 'tfidf_vectorizer.pkl')
-
-def get_access_token():
-    """Request headers에서 토큰을 받아오는 함수"""
-    access_token = request.headers.get('Authorization')
-    return access_token
 
 @app.route('/conversation', methods=['POST'])
 def conversation():
     request_data = request.get_json()
     text = request_data.get('text', '')
-    text_type = classifier.predict(text)
-    
-    # access_token을 받아옴
-    access_token = get_access_token()
-    
-    print(f"Authorization header: {access_token}")
-    print(f"text_type: {text_type}")
+    text_type = classifier.predict(text)  # 입력된 텍스트가 질문인지 진술인지 판별
+
+    # 클라이언트로부터 전달받은 토큰
+    access_token = request_data.get('token', '')
 
     # 세션을 영구적으로 설정하여 30분간 유지
     session.permanent = True
 
+    # 텍스트에서 옷의 특징을 추출
     features = extractor.extract_clothing_features(text)
-    
-        # 기본값 설정
-    default_top = {
-        "color": "WHITE",
-        "type": "SHORT",
-        "category": "COTTON",
-        "pattern": "SOLID"
-    }
-    default_bottom = {
-        "color": "BLACK",
-        "type": "LONG",
-        "category": "SLACKS",
-        "pattern": "SOLID"
-    }
-    print(f"features: {features}")
-    # 사용자가 상의를 말한 경우, 추출된 키워드는 그대로 유지하고 누락된 값만 기본값으로 채움
+
+    # 기본값 설정
+    default_top = {"color": "WHITE", "type": "SHORT", "category": "COTTON", "pattern": "SOLID"}
+    default_bottom = {"color": "BLACK", "type": "LONG", "category": "SLACKS", "pattern": "SOLID"}
+
+    # 상의와 하의 정보 처리
     top_features = []
     if 'top' in features:
-        cnt_top = 0
         for key, value in features['top'].items():
-            print(f"Key: {key}, Value: {value}")
-            if value == None:
-                features['top'][key]=default_top[key]
-                print(default_top[key])
-                cnt_top += 1
-            if cnt_top == 4:
-                features['top'] = None
-        
-    
+            if value is None:
+                features['top'][key] = default_top[key]
         top_features = features['top']
 
-    # 사용자가 하의를 말한 경우, 추출된 키워드는 그대로 유지하고 누락된 값만 기본값으로 채움
-    
     bottom_features = []
     if 'bottom' in features:
-        print("features1 : ", features['bottom'])
-        cnt_bottom = 0
         for key, value in features['bottom'].items():
-            print(f"Key: {key}, Value: {value}")
-            if value == None:
-                features['bottom'][key]=default_bottom[key]
-                print(default_bottom[key])
-                cnt_bottom += 1
-            if cnt_bottom == 4:
-                features['bottom'] = None
-        
-    
+            if value is None:
+                features['bottom'][key] = default_bottom[key]
         bottom_features = features['bottom']
 
-    print(f"top_features: {top_features}")
-    print(f"bottom_features: {bottom_features}")
-    print("-------------------")
-
+    # 진술문 처리
     if text_type == 'Statement':
-        # Statement: 사용자가 특정 옷을 입겠다는 의지를 표현한 경우
-        if top_features:  # 상의 정보가 있는 경우
-            session['top_features'] = top_features  # 세션에 상의 정보를 저장
-            session['top_clothesId'] = features.get('top', {}).get('clothesId', '')  # Spring에서 받아온 clothesId를 저장한다고 가정
+        if top_features:
+            session['top_features'] = top_features
+            session['top_clothesId'] = features.get('top', {}).get('clothesId', '')
+            if 'bottom_clothesId' in session:
+                # 코디가 완료되었을 때의 처리
+                session.pop('top_clothesId', None)
+                session.pop('bottom_clothesId', None)
+                return jsonify({"type": "Statement", "message": "코디가 완료되었습니다. 저장합니다."})
+            return jsonify({"type": "Statement", "message": "상의가 선택되었어요. 하의도 선택해주세요."})
 
-            if 'bottom_clothesId' in session:  # 상의 정보가 이미 있는 경우       
-                session.pop('top_clothesId', None)  # 세션에서 상의 정보 삭제
-                session.pop('bottom_clothesId', None)  # 세션에서 하의 정보 삭제
-                return jsonify({
-                    "type":"Statement",
-                    "message": "코디가 완료되었습니다. 저장합니다."
-                })
-            
-            return jsonify({
-                "type":"Statement",
-                "message": "상의가 선택되었어요. 하의도 선택해주세요."
-            })
+        elif bottom_features:
+            session['bottom_features'] = bottom_features
+            session['bottom_clothesId'] = features.get('bottom', {}).get('clothesId', '')
+            if 'top_clothesId' in session:
+                # 코디가 완료되었을 때의 처리
+                session.pop('top_clothesId', None)
+                session.pop('bottom_clothesId', None)
+                return jsonify({"type": "Statement", "message": "코디가 완료되었습니다. 저장합니다."})
+            return jsonify({"type": "Statement", "message": "하의가 선택되었어요. 상의도 선택해주세요."})
 
-        elif bottom_features:  # 하의 정보가 있는 경우
-            session['bottom_features'] = bottom_features  # 세션에 하의 정보를 저장
-            session['bottom_clothesId'] = features.get('bottom', {}).get('clothesId', '')  # Spring에서 받아온 clothesId를 저장한다고 가정
-
-            if 'top_clothesId' in session:  # 상의 정보가 이미 있는 경우       
-                session.pop('top_clothesId', None)  # 세션에서 상의 정보 삭제
-                session.pop('bottom_clothesId', None)  # 세션에서 하의 정보 삭제
-                return jsonify({
-                    "type":"Statement",
-                    "message": "코디가 완료되었습니다. 저장합니다."
-                })
-            else:
-                return jsonify({
-                    "type":"Statement",
-                    "message": "하의가 선택되었어요. 상의도 선택해주세요."
-                })
-
+    # 질문 처리
     elif text_type == 'Question':
-        # Question: 사용자가 조언을 묻는 경우
         spring_url = "https://i11a409.p.ssafy.io:8443/clothes/check"
-        spring_headers = {
-            'Content-Type': 'application/json;charset=UTF-8',
-            'Authorization': access_token,
-        }
+        spring_headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': access_token}
 
-        if top_features:  # 상의 정보가 있는 경우
-            session['top_features'] = top_features  # 세션에 상의 정보를 저장
+        if top_features:
+            session['top_features'] = top_features
             spring_data_top = {
                 "isTop": True,
                 "color": top_features.get('color', ''),
@@ -157,33 +94,23 @@ def conversation():
                 "category": top_features.get('category', ''),
                 "pattern": top_features.get('pattern', '')
             }
-
-            print("spring_data_top", spring_data_top)
             try:
                 response = requests.post(spring_url, headers=spring_headers, json=spring_data_top, verify=False)
-                # response = requests.get('https://i11a409.p.ssafy.io:8443/clothes/check', verify=False)
                 response.raise_for_status()
                 spring_response_data = response.json()
             except requests.exceptions.RequestException as e:
                 return jsonify({"error": str(e)}), 500
-            
-            # Spring에서 받아온 응답에 따라 처리
+
+            # 질문에 대한 응답 생성
             isAvailable = spring_response_data.get('isAvailable', False)
             session['top_clothesId'] = spring_response_data.get('clothesId', '')
-
             if isAvailable:
-                return jsonify({
-                    "type":"Question",
-                    "message": "입어도 됩니다. 입으시겠어요?"
-                })
+                return jsonify({"type": "Question", "message": "입어도 됩니다. 입으시겠어요?"})
             else:
-                return jsonify({
-                    "type":"Question",
-                    "message": "다른 걸 입는 걸 추천드려요. 그래도 입으시겠어요?"
-                })
+                return jsonify({"type": "Question", "message": "다른 걸 입는 걸 추천드려요. 그래도 입으시겠어요?"})
 
-        if bottom_features:  # 하의 정보가 있는 경우
-            session['bottom_features'] = bottom_features  # 세션에 하의 정보를 저장
+        if bottom_features:
+            session['bottom_features'] = bottom_features
             spring_data_bottom = {
                 "isTop": False,
                 "color": bottom_features.get('color', ''),
@@ -191,83 +118,65 @@ def conversation():
                 "category": bottom_features.get('category', ''),
                 "pattern": bottom_features.get('pattern', '')
             }
-
             try:
                 response = requests.post(spring_url, headers=spring_headers, json=spring_data_bottom, verify=False)
                 response.raise_for_status()
                 spring_response_data = response.json()
             except requests.exceptions.RequestException as e:
                 return jsonify({"error": str(e)}), 500
-            
-            # Spring에서 받아온 응답에 따라 처리
+
+            # 질문에 대한 응답 생성
             isAvailable = spring_response_data.get('isAvailable', False)
             session['bottom_clothesId'] = spring_response_data.get('clothesId', '')
-
             if isAvailable:
-                return jsonify({
-                    "type":"Question",
-                    "message": "입어도 됩니다. 입으시겠어요?"
-                })
+                return jsonify({"type": "Question", "message": "입어도 됩니다. 입으시겠어요?"})
             else:
-                return jsonify({
-                    "type":"Question",
-                    "message": "다른 걸 입는 걸 추천드려요. 그래도 입으시겠어요?"
-                })
+                return jsonify({"type": "Question", "message": "다른 걸 입는 걸 추천드려요. 그래도 입으시겠어요?"})
 
     return jsonify({"error": "잘못된 요청입니다."}), 400
 
 @app.route('/response', methods=['POST'])
 def handle_response():
-    data = request.json
-    response = data.get('text', '')
-    print(response)
-    
-    access_token = get_access_token()
-    
-    sentiment = predict_sentiment(response)
-    print(f"Sentiment: {sentiment}")
 
+    data = request.json
+    response = data.get('text', '')  # 사용자의 응답을 받음
+    access_token = data.get('token', '')  # 토큰을 받음
+
+    print(response)
+    sentiment = predict_sentiment(response)  # 응답에 대한 감정 분석
+
+    print(sentiment)
+
+    # 긍정적인 응답일 경우
     if sentiment == '긍정':
+        print("session",session)
         if 'top_clothesId' in session and 'bottom_clothesId' not in session:
-            return jsonify({
-                "message": "상의가 선택되었어요. 하의도 선택해주세요."
-            })
+            return jsonify({"message": "상의가 선택되었어요. 하의도 선택해주세요."})
 
         elif 'top_clothesId' not in session and 'bottom_clothesId' in session:
-            return jsonify({
-                "message": "하의가 선택되었어요. 상의도 선택해주세요."
-            })
+            return jsonify({"message": "하의가 선택되었어요. 상의도 선택해주세요."})
 
         elif 'top_clothesId' in session and 'bottom_clothesId' in session:
-            print(session)
             topId = session['top_clothesId']
             bottomId = session['bottom_clothesId']
             date = datetime.now().strftime("%Y-%m-%d")
 
-            spring_data = {
-                "topId": topId,
-                "bottomId": bottomId,
-                "date": date
-            }
-            print(spring_data)
-
+            spring_data = {"topId": topId, "bottomId": bottomId, "date": date}
             spring_url = "https://i11a409.p.ssafy.io:8443/coordinates"
-            spring_headers = {
-                'Content-Type': 'application/json',
-                'Authorization': access_token,
-            }
+            spring_headers = {'Content-Type': 'application/json', 'Authorization': access_token}
 
             try:
                 response = requests.post(spring_url, headers=spring_headers, json=spring_data, verify=False)
                 response.raise_for_status()
             except requests.exceptions.RequestException as e:
                 return jsonify({"error": str(e)}), 500
-            
-            session.pop('top_clothesId', None)  # 세션에서 상의 정보 삭제
-            session.pop('bottom_clothesId', None)  # 세션에서 하의 정보 삭제
 
+            # 코디 완료 후 세션 정리
+            session.pop('top_clothesId', None)
+            session.pop('bottom_clothesId', None)
             return jsonify({"message": "코디가 저장되었습니다."})
 
+    # 부정적인 응답일 경우
     elif sentiment == '부정':
         session.pop('top_clothesId', None)
         session.pop('bottom_clothesId', None)
@@ -275,11 +184,17 @@ def handle_response():
 
     return jsonify({"message": "다른 옷을 추천합니다."})
 
+
 ## 화자분석
 @app.route('/pyannote', methods=['POST'])
 def pyannote():
     global audio_files  # 전역 변수 사용 선언
 
+    # JSON 데이터에서 token 값을 가져옴
+    token_data = request.get_json()
+    token = token_data.get('token', '')
+
+    print(f"Received token: {token}")
     print(request.files)
     if 'audio' not in request.files:
         return jsonify({"error": "파일이 필요합니다."}), 400
@@ -299,11 +214,11 @@ def pyannote():
     return jsonify({"message": "음성 파일이 저장되었습니다."})
 
 
-def send_voice_data(voice_list, date):
+def send_voice_data(voice_list, date, token):
     url = "https://i11a409.p.ssafy.io:8443/voice"
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': app.access_token,
+        'Authorization': token,  # token 값을 헤더에 추가
     }
     data = {
         "voiceList": voice_list,
@@ -338,8 +253,8 @@ def process_audio_files():
         # 결과를 /voice 엔드포인트로 전송
         voice_list = result  # 여기서 result는 대화 시간 리스트라고 가정합니다
         date = datetime.now().strftime("%Y-%m-%d")
-        send_voice_data(voice_list, date)
-        
+        send_voice_data(voice_list, date, 'Bearer <token>')  # 여기에 실제 토큰을 전달
+
         # 처리 후 파일 삭제
         for file in audio_files:
             try:
@@ -368,4 +283,4 @@ if __name__ == '__main__':
     schedule_thread = threading.Thread(target=run_schedule)
     schedule_thread.start()
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
